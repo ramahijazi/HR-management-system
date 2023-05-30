395bdf026fcab4ee8f99f174d37b8588e8e4cd82
ramahijazi <ramahijazi95@outlook.com>
2023-05-30 11:29:13 +0000

    08

--- js/app.js
+++ js/app.js
@@ -2,159 +2,59 @@
 
 alert('welcome to our website')
 
+const allEmployee = [];
 
-let GhaziSamer = {
-    EmployeeID: X(),
-    FullName: 'Ghazi Samer',
-    Department: 'Administration',
-    Level: 'Senior',
-    MyImage: 'https://www.shutterstock.com/image-illustration/male-profile-picture-silhouette-avatar-260nw-149290403.jpg',
-    Salary: function () {
-        var min;
-        var max;
-        if (this.Level == 'Mid-Senior') { min = 1000; max = 1500 }
-        else if (this.Level == 'Senior') { min = 1500; max = 2000 }
-        else { min = 500; max = 1000 }
+function Employee(FullName, Department, Level,Salary) {
+    this.FullName = FullName,
+        // this.EmployeeID = X(),
+        this.Department = Department,
+        this.Level = Level,
+        // this.MyImage = MyImage,
+        this.Salary = 0,
+        this.newSalary = function () {
+        this.Salary = this.calSalary()
 
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        let salary = Math.floor(Math.random() * (max - min) + min);
-        let NetSalary = salary -(0.075 *salary);
-        return NetSalary
-    },
-    intro: function () { return `my name is ${this.FullName},my department is ${this.Department},my level is ${this.Level} ` }
+        }
+}
+// allEmployee.push(this) }
+
+
+
+
+let GhaziSamer = new Employee("Ghazi Samer", "Administration", "Senior");
+let LanaAli = new Employee("Lana Ali", "Finance", "Senior");
+let TamaraAyoub = new Employee("Tamara Ayoub","Marketing", "Senior");
+let SafiWalid = new Employee("Safi Walid",  "Administration", "Mid-Senio");
+let OmarZaid = new Employee("Omar Zaid",  "Development", "Senior");
+let RanaSaleh = new Employee("Rana Saleh",  "Development", "Junior");
+let HadiAhmad = new Employee("Hadi Ahmad",  "Finance", "Senior");
+console.log(allEmployee);
+
+// prototype 
+Employee.prototype.calSalary = function () {
+    var min = 0;
+    var max = 0;
+    if (this.Level == 'Mid-Senior') { min = 1000; max = 1500 }
+    // let salary = Math.floor(Math.random() * (max - min) + min);
+    // let NetSalary = salary -(0.075 *salary);
+    // return NetSalary}
+    else if (this.Level == 'Senior') { min = 1500; max = 2000 }
+    // let salary = Math.floor(Math.random() * (max - min) + min);
+    // let NetSalary = salary -(0.075 *salary);
+    // return NetSalary }
+    else {
+        min = 500; max = 1000
+
+        //min = Math.ceil(min);
+        // max = Math.floor(max); 
+    }
+
+    let salary = Math.floor(Math.random() * (max - min) + min);
+    let NetSalary = salary - (0.075 * salary);
+    return NetSalary
 }
 
-console.table(GhaziSamer);
-
-
-let LanaAli = {
-    EmployeeID: X(),
-    FullName: 'Lana Ali',
-    Department: 'Finance',
-    Level: 'Senior',
-    MyImage: 'https://static.vecteezy.com/system/resources/previews/009/952/670/original/female-profile-picture-vector.jpg',
-    Salary: function () {
-        var min;
-        var max;
-        if (this.Level == 'Mid-Senior') { min = 1000; max = 1500 }
-        else if (this.Level == 'Senior') { min = 1500; max = 2000 }
-        else { min = 500; max = 1000 }
-
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        let salary = Math.floor(Math.random() * (max - min) + min);
-        let NetSalary = salary -(0.075 *salary);
-        return NetSalary
-    },
-    intro: function () { return `my name is ${this.FullName},my department is ${this.Department},my level is ${this.Level}, ${this.MyImage}` }
-}
-
-console.table(LanaAli);
-
-
-let TamaraAyoub = {
-    EmployeeID: X(),
-    FullName: 'Tamara Ayoub',
-    Department: 'Marketing',
-    Level: 'Senior',
-    MyImage: 'https://static.vecteezy.com/system/resources/previews/009/952/670/original/female-profile-picture-vector.jpg',
-    Salary: function () {
-        var min;
-        var max;
-        if (this.Level == 'Mid-Senior') { min = 1000; max = 1500 }
-        else if (this.Level == 'Senior') { min = 1500; max = 2000 }
-        else { min = 500; max = 1000 }
-
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        let salary = Math.floor(Math.random() * (max - min) + min);
-        let NetSalary = salary -(0.075 *salary);
-        return NetSalary
-    },
-    intro: function () { return `my name is ${this.FullName},my department is ${this.Department},my level is ${this.Level}, ${this.MyImage}` }
-}
-
-console.table(TamaraAyoub);
-
-
-let SafiWalid = {
-    EmployeeID: X(),
-    FullName: 'Safi Walid',
-    Department: 'Administration',
-    Level: 'Mid-Senior',
-    MYImage: 'https://www.shutterstock.com/image-illustration/male-profile-picture-silhouette-avatar-260nw-149290403.jpg',
-    Salary: function () {
-        var min;
-        var max;
-        if (this.Level == 'Mid-Senior') { min = 1000; max = 1500 }
-        else if (this.Level == 'Senior') { min = 1500; max = 2000 }
-        else { min = 500; max = 1000 }
-
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        let salary = Math.floor(Math.random() * (max - min) + min);
-        let NetSalary = salary -(0.075 *salary);
-        return NetSalary
-    },
-    intro: function () { return `my name is ${this.FullName},my department is ${this.Department},my level is ${this.Level}, ${this.MyImage}` }
-}
-
-console.table(SafiWalid);
-
-
-
-let OmarZaid = {
-    EmployeeID: X(),
-    FullName: 'Omar Zaid',
-    Department: 'Development',
-    Level: 'Senior',
-    MyImage: 'https://www.shutterstock.com/image-illustration/male-profile-picture-silhouette-avatar-260nw-149290403.jpg',
-    Salary: function () {
-        var min;
-        var max;
-        if (this.Level == 'Mid-Senior') { min = 1000; max = 1500 }
-        else if (this.Level == 'Senior') { min = 1500; max = 2000 }
-        else { min = 500; max = 1000 }
-
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        let salary = Math.floor(Math.random() * (max - min) + min);
-        let NetSalary = salary -(0.075 *salary);
-        return NetSalary
-    },
-    intro: function () { return `my name is ${this.FullName},my department is ${this.Department},my level is ${this.Level}, ${this.MyImage}` }
-}
-
-console.table(OmarZaid);
-
-let RanaSaleh = {
-    EmployeeID: X(),
-    FullName: 'Rana Saleh',
-    Department: 'Development',
-    Level: 'Junior',
-    MyImage: 'https://static.vecteezy.com/system/resources/previews/009/952/670/original/female-profile-picture-vector.jpg',
-    Salary: function () {
-        var min;
-        var max;
-        if (this.Level == 'Mid-Senior') { min = 1000; max = 1500 }
-        else if (this.Level == 'Senior') { min = 1500; max = 2000 }
-        else { min = 500; max = 1000 }
-
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        let salary = Math.floor(Math.random() * (max - min) + min);
-        let NetSalary = salary -(0.075 *salary);
-        return NetSalary
-    },
-    intro: function () { return `my name is ${this.FullName},my department is ${this.Department},my level is ${this.Level}, ${this.MyImage}` }
-}
-
-console.table(RanaSaleh);
-
-
-
-function X() {// https://stackoverflow.com/questions/29640432/generate-4-digit-random-number-using-substring
+Employee.prototype.EmployeeID = function () {
     var a = Math.floor(1006 + Math.random() * 1000)
     a = a.toString().substring(0, 4);
 
@@ -165,65 +65,52 @@ function X() {// https://stackoverflow.com/questions/29640432/generate-4-digit-r
 
 
 
-let HadiAhmad = {
-        /*EmployeeID: function () {
-            random = Math.floor(Math.random() * 5);
-            console.log(random);
-            return random
-        }*/
-        EmployeeID: X(),
-        FullName: 'Hadi Ahmad',
-        Department: 'Finance',
-        Level: 'Senior',
-        MyImage: 'https://www.shutterstock.com/image-illustration/male-profile-picture-silhouette-avatar-260nw-149290403.jpg',
-        Salary: function () {
-            var min;
-            var max;
-            if (this.Level == 'Mid-Senior') { min = 1000; max = 1500 }
-            else if (this.Level == 'Senior') { min = 1500; max = 2000 }
-            else { min = 500; max = 1000 }
-
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            let salary = Math.floor(Math.random() * (max - min) + min);
-            let NetSalary = salary - (0.075 * salary);
-            return NetSalary
-        },
-        intro: function () {
-            return `Employee ID ${this.EmployeeID}, my name is ${this.FullName},
-my department is ${this.Department},my level is ${this.Level}`
-        }
-
-
-    }
+GhaziSamer.newSalary()
+LanaAli.newSalary()
+TamaraAyoub.newSalary()
+SafiWalid.newSalary()
+OmarZaid.newSalary()
+RanaSaleh.newSalary()
+HadiAhmad.newSalary()
 
 
 
-console.log(HadiAhmad.intro());
+Employee.prototype.renderEmployee = function () {
 
-console.log(HadiAhmad.Salary());
+    // let divE = document.createElement("div");
+    // divE.classList.add("card");
+    // let body = document.getElementsByTagName("body")[0];
+    // body.appendChild(divE);
+
+    // let EmployeeName = document.createElement("h2");
+    // EmployeeName.textContent = `Name: ${this.FullName}-`;
+    // divE.appendChild(EmployeeName);
+
+    // let EmployeeID = document.createElement("h3");
+    // EmployeeID.textContent = `ID: ${this.EmployeeID}`;
+    // divE.appendChild(EmployeeID);
+
+    // let EmployeeDepartment = document.createElement("h4");
+    // EmployeeDepartment.textContent = `Department: ${this.Department}`;
+    // divE.appendChild(EmployeeDepartment);
 
 
-console.table(HadiAhmad);
+    // let EmployeeImag = document.createElement("img");
+    // EmployeeImag.src = this.MyImage;
+    // divE.appendChild(EmployeeImag);
 
-document.getElementById("demo1").innerHTML = `Employee Name: ${GhaziSamer.FullName},
-Employee Salary: ${GhaziSamer.Salary()}`;
+    // let EmployeeSalary = document.createElement("h5");
+    // EmployeeSalary.textContent = ` salary : ${this.Salary}`;
+    // divE.appendChild(EmployeeSalary);
 
-document.getElementById("demo2").innerHTML = `Employee Name: ${LanaAli.FullName},
-Employee Salary: ${LanaAli.Salary()}`;
 
-document.getElementById("demo3").innerHTML = `Employee Name: ${TamaraAyoub.FullName},
-Employee Salary: ${TamaraAyoub.Salary()}`;
 
-document.getElementById("demo4").innerHTML = `Employee Name: ${SafiWalid.FullName},
-Employee Salary: ${SafiWalid.Salary()}`;
-
-document.getElementById("demo5").innerHTML = `Employee Name: ${OmarZaid.FullName},
-
-Employee Salary: ${OmarZaid.Salary()}`;
-
-document.getElementById("demo6").innerHTML = `Employee Name: ${RanaSaleh.FullName},
-Employee Salary: ${RanaSaleh.Salary()}`;
-
-document.getElementById("demo7").innerHTML = `Employee Name: ${HadiAhmad.FullName},
-Employee Salary: ${HadiAhmad.Salary()}`;
+        document.write(`<h1> Employee Name: ${this.FullName}<br>Department: ${this.Department} <br> Employee Salary: ${this.Salary} </h1> `)
+}
+GhaziSamer.renderEmployee()
+LanaAli.renderEmployee()
+TamaraAyoub.renderEmployee()
+SafiWalid.renderEmployee()
+OmarZaid.renderEmployee()
+RanaSaleh.renderEmployee()
+HadiAhmad.renderEmployee()
